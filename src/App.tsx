d4e0c99ec05e07dc0437de7672fcc4061d17c18b
Ahmet Sait Çalışır <ahmetsaidcalisir@gmail.com>
2023-05-09 19:25:53 +0300

    ReactIcon & GoogleFonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import mainSvg from "./assets/main.svg";
+import { TfiInstagram, TfiLocationPin, TfiMobile } from "react-icons/tfi";
 
 const App = () => {
   const [windowSize, setWindowSize] = useState({
@@ -24,30 +25,31 @@ const App = () => {
     <div className="main-contianer">
       <div className="modal">
         <div className="modal-title">
-          <img
-            src={mainSvg}
-            alt="React Logo"
-            style={{ maxWidth: windowSize.width, width: "100%" }}
-          />
+          <img src={mainSvg} alt="React Logo" width={windowSize.width * 0.7} />
         </div>
         <div className="modal-container">
-          <div className="modal-col">
-            <div className="col-title">İletişim Bilgilerimiz</div>
-          </div>
-          <div className="modal-col">
-            <div className="col-title">Adresimiz</div>
-            <div className="google-address-container">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3122.869449249637!2d27.726468276444002!3d38.49065427060046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b9a7dd42c0aaf7%3A0xe335b48f3cca703e!2sHEY*21%20CREATIVE%20Reklam%20Ajans%C4%B1!5e0!3m2!1sen!2str!4v1683218132982!5m2!1sen!2str"
-                loading="lazy"
-                className="google-address"
-              />
-            </div>
-            <div className="adress">
-              HEY! CREATIVE Reklam Ajansı, Ergenekon, Yavuz Sultan Selim Sokak
-              No:18 2/C D:304, 45400 Turgutlu/Manisa
-            </div>
-          </div>
+          <button
+            className="modal-button"
+            onClick={() => {
+              window.open("https://goo.gl/maps/SeuM1CNcpvJ11QWb9");
+            }}
+          >
+            <TfiLocationPin /> &nbsp;Konumumuz
+          </button>
+          <button
+            className="modal-button"
+            onClick={() => window.open("tel:+905010324391")}
+          >
+            <TfiMobile /> &nbsp;+90 (501) 032 43 91
+          </button>
+          <button
+            className="modal-button"
+            onClick={() => {
+              window.open("https://www.instagram.com/hey1creative");
+            }}
+          >
+            <TfiInstagram /> &nbsp;Instagram
+          </button>
         </div>
       </div>
     </div>
